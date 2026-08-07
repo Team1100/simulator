bebd9e2a1530847caa473d05b0846ed27053e50c
timerunner16 <calvin@koose.com>
2026-08-07 17:22:26 -0400

    include Lua and sol2 in the project
    Add Lua 5.4.8 as a submodule. Add sol2 3.5.0 as a submodule.
    Add a Lua test console in an ImGui window, built w/ sol2.
    Add gfx and lua state structs.
    Update readme.md dependency info.

--- src/main.cxx
+++ src/main.cxx
@@ -7,6 +7,100 @@
 
 #include "sokol_imgui.h"
 
+#include "sol/sol.hpp"
+
+// Holder of graphics state objects
+struct gfx_state_t {
+    sg_pass pass;
+} gfx;
+
+// Holder of Lua + console state
+struct lua_state_t {
+    std::array<char, 512> console_buf;
+    std::vector<std::string> console_out;
+    sol::state state;
+} lua;
+
+static void lua_print(sol::variadic_args va);
+static void reset_lua_state();
+static void show_console();
+static void init(void);
+static void frame(void);
+static void event(const sapp_event *event);
+static void cleanup(void);
+
+// Custom Lua print function to print to the UI console rather than stdout
+static void lua_print(sol::variadic_args va) {
+    std::string str;
+    for (auto v : va) {
+        // use Lua's tostring to get a string, then convert to std::string
+        auto vstr = lua.state["tostring"](v.as<sol::object>()).get<std::string>();
+        str += vstr + "\t";
+    }
+    lua.console_out.push_back(str);
+}
+
+// (Re)start Lua state and prepare environment
+static void reset_lua_state() {
+    // Reset state
+    lua.state = sol::state();
+    // Open only core libraries (no I/O, OS, etc.)
+    lua.state.open_libraries(sol::lib::base, sol::lib::math, sol::lib::string);
+    // Override print with custom one for console output
+    lua.state["print"] = lua_print;
+}
+
+// Display the Lua console with multi-line input, output, and controls
+static void show_console() {
+    if (ImGui::Begin("Lua Test Console")) {
+        // World's worst code editor
+        ImGui::InputTextMultiline("Script Input", lua.console_buf.data(), lua.console_buf.size(),
+                                  ImVec2(0, 0), ImGuiInputTextFlags_AllowTabInput);
+
+        // Character counter
+        std::string_view str(lua.console_buf.data());
+        ImGui::Text("%zu/%zu", str.size(), lua.console_buf.size());
+
+        // Safely execute script and output errors to the console
+        if (ImGui::Button("Execute")) {
+            auto res = lua.state.safe_script(str, sol::script_pass_on_error);
+            if (!res.valid()) {
+                sol::error err = res;
+                lua.console_out.push_back(std::string("[error] ") + err.what());
+            }
+        }
+
+        ImGui::SameLine();
+
+        // Globals persist between script executions, resetting clears them
+        if (ImGui::Button("Reset Lua"))
+            reset_lua_state();
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Clear Output"))
+            lua.console_out.clear();
+
+        ImGui::Separator();
+
+        // Formatted output, each message is a separate widget
+        if (ImGui::BeginChild("Output", ImVec2(0, ImGui::GetContentRegionAvail().y),
+                              ImGuiChildFlags_NavFlattened, ImGuiWindowFlags_HorizontalScrollbar)) {
+            for (auto i : lua.console_out) {
+                if (i.starts_with("[error]"))
+                    ImGui::TextColored({1.0f, 0.4f, 0.2f, 1.0f}, "%s", i.c_str());
+                else
+                    ImGui::Text("%s", i.c_str());
+            }
+
+            if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+                ImGui::SetScrollHereY(1.0f);
+        }
+        ImGui::EndChild();
+    }
+    ImGui::End();
+}
+
 static void init(void) {
     sg_desc gfx_desc{};
     gfx_desc.environment = sglue_environment();
@@ -20,14 +114,17 @@ static void init(void) {
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    gfx.pass = sg_pass{};
+    gfx.pass.action.colors[0] = {
+        .load_action = SG_LOADACTION_CLEAR,
+        .clear_value = {0.15f, 0.18f, 0.2f, 1.0f},
+    };
+    reset_lua_state();
 }
 
 static void frame(void) {
-    sg_pass pass{};
-    pass.action.colors[0] = {
-        .load_action = SG_LOADACTION_CLEAR, .clear_value = {0.15f, 0.18f, 0.2f, 1.0f}
-    };
-    pass.swapchain = sglue_swapchain();
+    gfx.pass.swapchain = sglue_swapchain();
 
     simgui_frame_desc_t imgui_fdesc{
         .width = sapp_width(),
@@ -40,7 +137,9 @@ static void frame(void) {
     ImGui::DockSpaceOverViewport(0, nullptr, ImGuiDockNodeFlags_PassthruCentralNode);
     ImGui::ShowDemoWindow();
 
-    sg_begin_pass(&pass);
+    show_console();
+
+    sg_begin_pass(&gfx.pass);
     simgui_render();
     sg_end_pass();
     sg_commit();
